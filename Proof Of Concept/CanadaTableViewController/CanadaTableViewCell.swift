92a2945132d4b39fcaa5244fc04caa215cd54845
Trupti Gavhane <gavhane.trupti@tcs.com>
2018-07-07 12:32:34 +0530

    "Committing to master repo with basic app skeleton with one page and a nav bar. Removed main.storyboard"

---
+++ Proof Of Concept/CanadaTableViewController/CanadaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CanadaTableViewCell.swift
+//  Proof Of Concept
+//
+//  Created by Trupti Gavhane on 07/07/18.
+//  Copyright © 2018 Telstra. All rights reserved.
+//
+
+import UIKit
+
+class CanadaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
